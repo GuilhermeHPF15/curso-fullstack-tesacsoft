ce484622aaf238f602b6f5fd476fc90732321b40
MorganaAPF <morganaapf@gmail.com>
2026-03-02 18:49:15 +0100

    add aula03 - ex003

---
+++ aula05/ex003.java
@@ -0,0 +1,32 @@
+/*
+Menu de Opções:
+
+Exiba um menu simples: "1. Iniciar Jogo", "2. Carregar Jogo", "3. Sair". Peça ao utilizador para escolher uma ação.
+ */
+
+import java.util.Scanner;
+
+public class ex003 {
+    public static void main(String[] args) {
+        int escolha;
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("1. Iniciar Jogo\n2. Carregar Jogo\n3. Sair\n");
+        System.out.print("Sua escolha: ");
+        escolha = leitor.nextInt();
+
+        switch (escolha) {
+            case 1:
+                System.out.println("Jogo iniciado!");
+                break;
+            case 2:
+                System.out.println("Jogo carregado!");
+                break;
+            case 3:
+                System.out.println("Jogo encerrado!");
+                break;
+            default:
+                System.out.println("Opção inválida!");
+        }
+    }
+}
