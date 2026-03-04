daeeb93c84405e010f560c74cf3c1166a6e5ac00
MorganaAPF <morganaapf@gmail.com>
2026-03-04 19:00:03 +0100

    refactor: aula05/ex002
    - Otimiza a localidade do código.
    - Deleta variáveis desnecessárias.
    - Otimiza os tipos das variáveis.
    - Otimiza o DRY.
    - Otimiza a formatação dos outputs.

--- aula05/ex002.java
+++ aula05/ex002.java
@@ -12,40 +12,36 @@ import java.util.Scanner;
 
 public class ex002 {
     public static void main(String[] args) {
-        double num1, num2;
-        String n1f, n2f, resultado;
-        char operador;
         DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
         DecimalFormat df = new DecimalFormat("0.##", symbols);
         Scanner leitor = new Scanner(System.in);
         leitor.useLocale(Locale.US);
 
+        double num1, num2;
+        char operador;
         System.out.print("Primeiro número: ");
         num1 = leitor.nextDouble();
-
         System.out.print("Operador: ");
         operador = leitor.next().charAt(0);
-
         System.out.print("Segundo número: ");
         num2 = leitor.nextDouble();
+        leitor.close();
 
-        n1f = df.format(num1);
-        n2f = df.format(num2);
-
+        double resultado;
         switch (operador) {
-            case '+': resultado = df.format(num1 + num2); break;
-            case '-': resultado = df.format(num1 - num2); break;
-            case '*': resultado = df.format(num1 * num2); break;
+            case '+': resultado = num1 + num2; break;
+            case '-': resultado = num1 - num2; break;
+            case '*': resultado = num1 * num2; break;
             case '/':
                 if (num2 == 0) {
                     System.out.println("Não se pode dividir por zero!");
                     return;
                 }
-                resultado = df.format(num1 / num2);
+                resultado = num1 / num2;
                 break;
             default: System.out.println("\nOperador inválido!"); return;
         }
 
-        System.out.println(n1f + " " + operador + " " + n2f + " = " + resultado);
+        System.out.printf("%s %c %s = %s%n", df.format(num1), operador, df.format(num2), df.format(resultado));
     }
 }
