6aaf77ce7f112bed3a838e27e5327e8d1a99f335
MorganaAPF <morganaapf@gmail.com>
2026-04-17 22:51:03 +0100

    feat: add aula 21 and refactor aula 20

--- aula20/ex005/script.js
+++ aula20/ex005/script.js
@@ -1,32 +1,133 @@
 let textBox = document.querySelector("input");
-let p = document.querySelector("p");
+let p = document.querySelector("#n1");
+let p2 = document.querySelector("#n2");
 let sinal = document.querySelector("#sinal");
+let resultado = 0;
+let final = false;
+
+function MoM() {
+    textBox.value *= -1;
+}
+
+function CE() {
+    textBox.value = 0;
+}
+
+function C() {
+    textBox.value = 0;
+    p.innerText = "";
+    p2.innerText = "";
+    sinal.innerText = "";
+}
+
+function del() {
+    textBox.value = textBox.value.slice(0, -1);
+}
 
 function enterNumber(valor) {
-    textBox.value += valor;
+    if (final) {
+        p.innerText = "";
+        p2.innerText = "";
+        sinal.innerText = "";
+        textBox.value = "0";
+        final = false;
+    }
+
+    if (textBox.value == 0) {
+        textBox.value = valor;
+    } else {
+        textBox.value += valor;
+    }
 }
 
 function operacao(acao) {
-    p.innerText = textBox.value;
-        if (acao == "soma") {
-            let valor = textBox.value;
-            textBox.value = "0";
-            sinal.innerText = "+";
+    if (final) {
+        p.innerText = textBox.value;
+        p2.innerText = "";
+        sinal.innerText = "";
+        final = false;
+    }
+
+    if (sinal.innerText == "") {
+        resultado = parseFloat(textBox.value);
+    }
+
+    else if (sinal.innerText == "+") {
+        resultado = parseFloat(textBox.value) + parseFloat(p.innerText);
+    }
+
+    else if (sinal.innerText == "-") {
+        if (p.innerText == 0) {
+            resultado = parseFloat(textBox.value) - parseFloat(p.innerText);
+        } else {
+            resultado = parseFloat(p.innerText) - parseFloat(textBox.value);
         }
+    }
+
+    else if (sinal.innerText == "x") {
+        if (p.innerText == 0) {
+            resultado = parseFloat(textBox.value) * 1;
+        } else {
+            resultado = parseFloat(p.innerText) * parseFloat(textBox.value);
+        }
+    }
+
+    else if (sinal.innerText == "/") {
+        if (p.innerText == 0) {
+            resultado = parseFloat(textBox.value) / 1;
+        } else {
+            resultado = parseFloat(p.innerText) / parseFloat(textBox.value);
+        }
+    }
+
+    p.innerText = resultado;
+    textBox.value = "0";
+    
+    if (acao == "soma") {
+        sinal.innerText = "+";
+    }
+
+    if (acao == "sub") {
+        sinal.innerText = "-";
+    }
+    
+    if (acao == "mult") {
+        sinal.innerText = "x";
+    }
+
+    if (acao == "div") {
+        sinal.innerText = "/";
+    }
 }
 
 function igual() {
-    let resultado = 0;
-    if (sinal.innerText == "+") {
-        resultado = parseFloat(p.innerText) + parseFloat(textBox.value);
-    } else if (sinal.innerText == "-") {
-        resultado = parseFloat(p.innerText) - parseFloat(textBox.value);
-    } else if (sinal.innerText == "x") {
-        resultado = parseFloat(p.innerText) * parseFloat(textBox.value);
-    } else if (sinal.innerText == "/") {
-        resultado = parseFloat(p.innerText) / parseFloat(textBox.value);
-    }
+    if (final) {
+        p.innerText = textBox.value;
 
-    textBox.value = resultado;
-    p.innerText = "";
+        if (sinal.innerText == "+") {
+            resultado = parseFloat(textBox.value) + parseFloat(p2.innerText);
+        } else if (sinal.innerText == "-") {
+            resultado = parseFloat(textBox.value) - parseFloat(p2.innerText);
+        } else if (sinal.innerText == "x") {
+            resultado = parseFloat(textBox.value) * parseFloat(p2.innerText);
+        } else if (sinal.innerText == "/") {
+            resultado = parseFloat(textBox.value) / parseFloat(p2.innerText);
+        }
+
+        textBox.value = resultado;
+    } else {
+        if (sinal.innerText == "+") {
+            resultado = parseFloat(p.innerText) + parseFloat(textBox.value);
+        } else if (sinal.innerText == "-") {
+            resultado = parseFloat(p.innerText) - parseFloat(textBox.value);
+        } else if (sinal.innerText == "x") {
+            resultado = parseFloat(p.innerText) * parseFloat(textBox.value);
+        } else if (sinal.innerText == "/") {
+            resultado = parseFloat(p.innerText) / parseFloat(textBox.value);
+        }
+
+        p2.innerText += textBox.value;
+        textBox.value = resultado;
+        final = true;
+    }
 }
---
+++ aula21/ex001/script.js
@@ -0,0 +1,3 @@
+let img = document.createElement("img");
+img.src = "on.jpeg";
+document.querySelector("#ultimaDiv").appendChild(img);
