b530ce25a1764c81cbc23dae8bd61a914fd839bd
MorganaAPF <morganaapf@gmail.com>
2026-04-24 02:04:33 +0100

    refactor: aula24\ex002 - melhorias

--- aula24/ex002/table.js
+++ aula24/ex002/table.js
@@ -5,31 +5,38 @@ let color = document.querySelector("#color");
 let flavor = document.querySelector("#flavor");
 let origin = document.querySelector("#origin");
 
-if (localStorage.getItem("frutas")) {
-    let printFruit = JSON.parse(localStorage.getItem("frutas"));
-    printFruit.forEach(fruit => {
-        let newRow = document.createElement("tr");
+function insertInto(object) {
+    let newRow = document.createElement("tr");
         table.appendChild(newRow);
-        Object.values(fruit).forEach(fruitElement => {
-            newData = document.createElement("td");
-            newData.innerText = fruitElement;
+        Object.values(object).forEach(element => {
+            let newData = document.createElement("td");
+            newData.innerText = element;
             newRow.appendChild(newData);
-        })
+        });
+}
+
+if (localStorage.getItem("frutas")) {
+    let getFruit = JSON.parse(localStorage.getItem("frutas"));
+    getFruit.forEach(fruit => {
+        insertInto(fruit);
     });
 }
 
 function submit() {
-    let fruit = {id: Date.now(), nome: name.value, cor: color.value, sabor: flavor.value, origem: origin.value};
+    let fruitArray = []
+    let fruit = {id: 0, nome: name.value, cor: color.value, sabor: flavor.value, origem: origin.value};
     if (!localStorage.getItem("frutas")) {
-        let fruitArray = []
+        fruit["id"] = 1;
         fruitArray.push(fruit);
         localStorage.setItem("frutas", JSON.stringify(fruitArray));
     } else {
         let getFruit = JSON.parse(localStorage.getItem("frutas"));
+        fruit["id"] = getFruit.length + 1;
         getFruit.push(fruit);
         localStorage.setItem("frutas", JSON.stringify(getFruit));
     }
-    table.innerHTML += `<tr><td>${fruit["id"]}</td><td>${fruit["nome"]}</td><td>${fruit["cor"]}</td><td>${fruit["sabor"]}</td><td>${fruit["origin"]}</td><td><button>Apagar</button></td></tr>`
+        insertInto(fruit);
+        name.value = color.value = flavor.value = origin.value = "";
 }
 
 //localStorage.clear();
