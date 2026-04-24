7e85d95940b9e0cc5ae698d319452004d622498a
MorganaAPF <morganaapf@gmail.com>
2026-04-24 03:36:32 +0100

    refactor: aula24\ex002 - v1.0 concluída

--- aula24/ex002/table.js
+++ aula24/ex002/table.js
@@ -7,19 +7,19 @@ let origin = document.querySelector("#origin");
 
 function insertInto(object) {
     let newRow = document.createElement("tr");
-        table.appendChild(newRow);
-        Object.values(object).forEach(element => {
-            let newData = document.createElement("td");
-            newData.innerText = element;
-            newRow.appendChild(newData);
-        });
-}
-
-if (localStorage.getItem("frutas")) {
-    let getFruit = JSON.parse(localStorage.getItem("frutas"));
-    getFruit.forEach(fruit => {
-        insertInto(fruit);
+    newRow.dataset.id = object.id;
+    table.appendChild(newRow);
+    Object.values(object).forEach(element => {
+        let newData = document.createElement("td");
+        newData.innerText = element;
+        newRow.appendChild(newData);
     });
+    let tdButton = document.createElement("td");
+    newRow.appendChild(tdButton);
+    let button = document.createElement("button");
+    button.innerText = "Apagar";
+    tdButton.appendChild(button);
+    button.addEventListener("click", erase);
 }
 
 function submit() {
@@ -31,7 +31,7 @@ function submit() {
         localStorage.setItem("frutas", JSON.stringify(fruitArray));
     } else {
         let getFruit = JSON.parse(localStorage.getItem("frutas"));
-        fruit["id"] = getFruit.length + 1;
+        fruit["id"] = getFruit.length > 0 ? getFruit[getFruit.length -1]["id"] + 1 : 1;
         getFruit.push(fruit);
         localStorage.setItem("frutas", JSON.stringify(getFruit));
     }
@@ -39,4 +39,20 @@ function submit() {
         name.value = color.value = flavor.value = origin.value = "";
 }
 
+function erase(event) {
+    let row = event.target.closest("tr");
+    let targetId = row.dataset.id;
+    row.remove();
+    let fruits = JSON.parse(localStorage.getItem("frutas"));
+    let removeFruit = fruits.filter(fruit => fruit.id != targetId);
+    localStorage.setItem("frutas", JSON.stringify(removeFruit));
+}
+
 //localStorage.clear();
+
+if (localStorage.getItem("frutas")) {
+    let getFruit = JSON.parse(localStorage.getItem("frutas"));
+    getFruit.forEach(fruit => {
+        insertInto(fruit);
+    });
+}
